af4616115f18e2177e4cd1e8e62a5db060c7652e
Lucas Barbosa <barbosa.lucas1996@gmail.com>
2018-11-08 21:44:40 -0200

    Criando variavel e função (feed_page)

--- myApp/src/pages/feed/feed.ts
+++ myApp/src/pages/feed/feed.ts
@@ -14,12 +14,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'feed.html',
 })
 export class FeedPage {
+  public nome_usuario:string = "Lucas Barbosa";
+  //sem o public é padrão, o tipo de variavel é declardo para que a var receba apenas oque ela espera.
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  public somaDoisNumeros(): void {
+    alert(5 + 10);
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad FeedPage');
+    this.somaDoisNumeros();
   }
 
 }
